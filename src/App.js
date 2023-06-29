45d301545fea476bd7955f725524709764645dda
rahulchapter <rahul.t@chapter247.com>
2023-06-29 16:32:16 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+
+import { useEffect, useState } from "react";
+import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
+
+// Put your API key here
+builder.init('dceb8d32df5e4c2ea3b1f5f0e12530a1');
+
+function App() {
+  const isPreviewingInBuilder = useIsPreviewing();
+  const [notFound, setNotFound] = useState(false);
+  const [content, setContent] = useState(null);
+
+  // get the page content from Builder
+   useEffect(() => {
+    async function fetchContent() {
+      const content = await builder
+        .get("page", {
+          url: window.location.pathname
+        })
+        .promise();
+
+      setContent(content);
+      setNotFound(!content);
+
+      // if the page title is found, 
+      // set the document title
+      if (content?.data.title) {
+       document.title = content.data.title
+      }
+    }
+    fetchContent();
+  }, [window.location.pathname]);
+  
+  // If no page is found, return 
+  // a 404 page from your code.
+  // The following hypothetical 
+  // <FourOhFour> is placeholder.
+  /* if (notFound && !isPreviewingInBuilder) {
+    return <FourOhFour/>
+  } */
+
+  // return the page when found
+  return (
+    <>
+      {/* Render the Builder page */}
+      <BuilderComponent model="page" content={content} />
+    </>
+  );
+}
+
+export default App;
